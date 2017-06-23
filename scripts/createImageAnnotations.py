a28c349cccadd51d13c2749762ddc88c979c2f45
Yash Choukse <yash.choukse123@gmail.com>
2017-06-23 17:53:20 +0530

    fixed CursorNotFound error

--- scripts/createImageAnnotations.py
+++ scripts/createImageAnnotations.py
@@ -8,19 +8,31 @@ db = client.dataset
 labels = db.labels
 dictCollection = db.dict
 destinationCollection = db.imageAnnotations
-cursor = labels.find({})
+cursor = labels.find({}).batch_size(30)
 
-for document in cursor:
-	imageID = document['ImageID']
-	labelName = document['LabelName']
-	result = dictCollection.find_one({"dictID": labelName})
-	if result:
-		splittedLabels = result['label'].split(' ')
-		for i in splittedLabels:
-			destinationCollection.update(
-				{
-					"imageID" : imageID
-				},
-				{
-					"$push": {"annotations": i}
-				}, upsert=True)
+labelRecordsCount =  cursor.count()
+
+counter = 0
+
+try:
+	for document in cursor:
+		counter = counter + 1
+		print " ",counter,"/",labelRecordsCount," \r",
+
+		imageID = document['ImageID']
+		labelName = document['LabelName']
+		result = dictCollection.find_one({"dictID": labelName})
+		if result:
+			splittedLabels = result['label'].split(' ')
+			for i in splittedLabels:
+				destinationCollection.update(
+					{
+						"imageID" : imageID
+					},
+					{
+						"$push": {"annotations": i}
+					}, upsert=True)
+	print counter
+except:
+	print counter
+cursor.close()
