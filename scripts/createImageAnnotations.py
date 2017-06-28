03a34a19d8521efb4f288e1dfedede303d323521
Yash Choukse <yash.choukse123@gmail.com>
2017-06-28 21:11:48 +0530

    Script for imageAnnotations collection

--- scripts/createImageAnnotations.py
+++ scripts/createImageAnnotations.py
@@ -13,26 +13,30 @@ cursor = labels.find({}).batch_size(30)
 labelRecordsCount =  cursor.count()
 
 counter = 0
-
+lastBreak = 1026150
 try:
 	for document in cursor:
 		counter = counter + 1
-		print " ",counter,"/",labelRecordsCount," \r",
+		if counter > lastBreak:
+			print " ",counter,"/",labelRecordsCount," \r",
 
-		imageID = document['ImageID']
-		labelName = document['LabelName']
-		result = dictCollection.find_one({"dictID": labelName})
-		if result:
-			splittedLabels = result['label'].split(' ')
-			for i in splittedLabels:
-				destinationCollection.update(
-					{
-						"imageID" : imageID
-					},
-					{
-						"$push": {"annotations": i}
-					}, upsert=True)
+			imageID = document['ImageID']
+			labelName = document['LabelName']
+			result = dictCollection.find_one({"dictID": labelName})
+			if result:
+				splittedLabels = result['label'].split(' ')
+				for i in splittedLabels:
+					destinationCollection.update(
+						{
+							"imageID" : imageID
+						},
+						{
+							"$push": {"annotations": i}
+						}, upsert=True)
+	print " "
 	print counter
+
 except:
+	print " "
 	print counter
 cursor.close()
